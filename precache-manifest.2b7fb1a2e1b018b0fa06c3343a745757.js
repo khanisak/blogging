172bf7b5b003d9aae9ccd3f6b0deb4ca63794e49
Khanis Anthony <khanis.anthony@angkasa.net.id>
2019-07-10 10:22:53 +0700

    Updates

---
+++ precache-manifest.2b7fb1a2e1b018b0fa06c3343a745757.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75bdbe37e08ef1352c2a60e8b4df135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7518d9b20f766031c55",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "693625fa3e19b094d952",
+    "url": "/static/js/2.efa0e3ac.chunk.js"
+  },
+  {
+    "revision": "d7518d9b20f766031c55",
+    "url": "/static/js/main.62353d20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
